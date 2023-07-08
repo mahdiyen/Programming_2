13a32749f356ad3293ed847a06969d9120853752
mahdiyen <mnabilou@st.hanze.nl>
2023-07-08 13:34:52 +0200

    thenetwork_client  code modified

--- assignment_week_2.1/assignment_week_2.1_network_client.py
+++ assignment_week_2.1/assignment_week_2.1_network_client.py
@@ -6,6 +6,10 @@ class NetworkClient:
         self.base_url = base_url
 
     async def fetch_data(self, endpoint):
+        """
+        Fetches data from the given endpoint by sending an HTTP GET request.
+        Returns the response data as JSON if the status is 200, otherwise returns None.
+        """
         async with aiohttp.ClientSession() as session:
             async with session.get(self.base_url + endpoint) as response:
                 if response.status == 200:
@@ -14,6 +18,11 @@ class NetworkClient:
                     return None
 
     async def process_data(self, endpoint, callback):
+        """
+        Fetches data from the given endpoint using fetch_data() method,
+        and then calls the provided callback function on the retrieved data.
+        Returns the result of the callback function.
+        """
         data = await self.fetch_data(endpoint)
         if data:
             return await callback(data)
@@ -22,15 +31,21 @@ class NetworkClient:
 
 
 async def calculate_average_temperature(data):
-    # Example function to calculate the average temperature from the data
+    '''
+    calculates the average temperature from the data.
+    Receives data as input and returns a dictionary with the average temperature.
+    '''
     temperatures = [float(entry['J-D']) for entry in data]
     return {'average_temperature': sum(temperatures) / len(temperatures)}
 
 async def print_data(data):
-    # Example function to print the received data
+    """
+    Receives data as input and prints it.
+    """    
     print(data)
 
 async def main():
+
     base_url = 'http://localhost:8080/data/'
     client = NetworkClient(base_url)
 
